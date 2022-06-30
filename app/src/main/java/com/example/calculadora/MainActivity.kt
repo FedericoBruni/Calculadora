2698da25fe1632b0b5b95b378fb8c257aa967b60
FedericoBruni <fbruni@fi.uba.ar>
2022-06-30 19:08:53 -0300

    Agregado botón accept en PP

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -12,6 +12,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
--- app/src/main/java/com/example/calculadora/PrivacyPolicyDialogFragment.kt
+++ app/src/main/java/com/example/calculadora/PrivacyPolicyDialogFragment.kt
@@ -1,13 +1,17 @@
 package com.example.calculadora
 
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.DialogInterface
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
@@ -31,6 +35,10 @@ class PrivacyPolicyDialogFragment: DialogFragment() {
             //val height = ViewGroup.LayoutParams.MATCH_PARENT
             dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
             //dialog.getWindow()?.setLayout(width, height)
+            val acceptButton = dialog.findViewById<Button>(R.id.accept_pp_btn)
+            acceptButton.setOnClickListener{
+                dialog.dismiss()
+            }
         }
     }
 }
